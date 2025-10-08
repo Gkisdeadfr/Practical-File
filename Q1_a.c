28bb6f3fe3f285fd2cbaace324f40ab5d95a4667
Gkisdeadfr <gunagya.kochar@gmail.com>
2025-10-08 22:08:52 +0530

    Question - 1

---
+++ Q1_a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int a,b,c;
+  printf("Enter 1st number: ");
+  scanf("%d", &a);
+  printf("Enter 2nd number: ");
+  scanf("%d", &b);
+  c=a+b;
+  printf("The sum of the two numbers is: %d", c);
+ 
+}
---
+++ Q1_b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int a,b,c,d;
+  printf("Enter 1st number: ");
+  scanf("%d", &a);
+  printf("Enter 2nd number: ");
+  scanf("%d", &b); 
+  printf("Enter 3rd number: ");
+  scanf("%d", &c); 
+  d=a+b+c;
+  printf("The sum of the three numbers is: %d", d);
+ 
+}
