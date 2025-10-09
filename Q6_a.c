f5fd941b7e98005de0e32b16eb28a6c9f323369e
Gkisdeadfr <gunagya.kochar@gmail.com>
2025-10-09 08:14:11 +0530

    added q6-a

---
+++ Q6_a.c
@@ -0,0 +1,15 @@
+// Write a C program to convert a given integer (in seconds) to hours, minutes, and seconds.
+// THE INTEGRAL INPUT IS IN SECONDS!!!!!!!!!!!!!
+
+#include <stdio.h>
+int main(){
+  int seconds, h,m,s;
+  printf("Enter the time in seconds: ");
+  scanf("%d", &seconds);
+  h = (seconds/3600);
+  m = (seconds -(3600*h))/60;
+  s = (seconds -(3600*h)-(60*m));
+  printf("The time in H:M:S is:: %d:%d:%d", h,m,s);
+
+  return 0;
+}
